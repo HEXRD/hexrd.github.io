862b6ca3c42304ecf154d9be9608a65bcc9316bf
Kevin Welsh <kevin.welsh@verdantevolution.com>
2024-12-04 11:01:12 -0500

    add a couple of images and some more reasonable text. Remove the bios for now.

--- src/components/GuiSection.js
+++ src/components/GuiSection.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React from "react";
 
 const GuiSection = () => (
-  <section id="gui" style={{ padding: '2rem' }}>
-    <h2>HEXRD GUI</h2>
-    <p>The HEXRD GUI offers a streamlined interface for...</p>
-  </section>
+    <section id="gui" style={{ padding: "2rem" }}>
+        <h2>Using HEXRD</h2>
+        <p>
+            HEXRD is primarily used through the{" "}
+            <a href="https://hexrdgui.readthedocs.io/en/latest/">
+                HEXRD User Interface
+            </a>
+            . The HEXRD GUI provides many interactive workflows for visualizing
+            and processing X-ray diffraction image data images using the generic
+            multi-detector instrument model in HEXRD.
+        </p>
+        <div
+            style={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                height: "30em",
+                justifyContent: "space-evenly",
+                marginTop: "2em",
+            }}
+        >
+            <img
+                style={{
+                    height: "100%",
+                }}
+                src="images/rotation_series.png"
+                alt="Diffraction"
+            />
+            <img
+                style={{
+                    height: "100%",
+                }}
+                src="images/tardis_powder_and_laue_overlays.png"
+                alt="Tardis powder and laue overlays"
+            />
+        </div>
+    </section>
 );
 
 export default GuiSection;
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const Introduction = () => (
-  <section id="introduction" style={{ padding: '2rem' }}>
-    <h2>About HEXRD</h2>
-    <p>HEXRD is an open-source library focused on high-energy X-ray diffraction analysis...</p>
-  </section>
+    <section id="introduction" style={{ padding: "2rem" }}>
+        <h2>About HEXRD</h2>
+        <p>
+            The HEXRD project is developing a cross-platform, open-source
+            library for the general analysis of X-ray diffraction data. This
+            includes powder diffraction, Laue diffraction, and High Energy
+            Diffraction Microscopy (a.k.a. 3DXRD, multi-grain rotation method)
+            modalities. At its core, HEXRD provides an abstraction of a generic
+            diffraction instrument with support for multiple detectors. This
+            includes optimized transforms from the direct and reciprocal crystal
+            lattices to the local detector coordinates, harnesses for
+            interpolating image data into scattering angle coordinates, and
+            sophisticated calibration routines.
+        </p>
+    </section>
 );
 
 export default Introduction;
